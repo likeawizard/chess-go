1496dd9ce0c9baa7c50117841382999f2975c0b6
Arturs Priede <arturs.priede88@gmail.com>
2022-09-28 11:19:23 +0300

    faet: improved PST, mate detection in search

--- build.sh
+++
@@ -1,5 +0,0 @@
-go build -o lichess cmd/lichess/main.go
-go build -o self-play cmd/self-play/main.go
-go build -o analyze cmd/analyze/main.go
-go build -o single-eval cmd/single-eval/main.go
-go build -o perft cmd/perft/main.go
