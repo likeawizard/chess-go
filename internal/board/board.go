9bec8753ebe21479b2ec68342d9e3e7bd5bc46d3
Arturs Priede <arturs.priede88@gmail.com>
2022-10-03 22:23:40 +0300

    feat: MVV-LVA move ordering

--- internal/board/board.go
+++ internal/board/board.go
@@ -1,8 +1,6 @@
 package board
 
 import (
-	"sort"
-
 	"github.com/likeawizard/chess-go/internal/config"
 )
 
@@ -33,38 +31,6 @@ func (b *Board) Copy() *Board {
 	}
 
 	return &copy
-}
-
-func (b *Board) OrderMoves(pv Move, moves *[]Move) {
-	sort.Slice(*moves, func(i int, j int) bool {
-		return (*moves)[i] == pv || b.getMoveValue((*moves)[i]) > b.getMoveValue((*moves)[j])
-	})
-}
-
-// Estimate the potential strength of the move for move ordering
-func (b *Board) getMoveValue(move Move) (value float32) {
-
-	dir := float32(-1)
-	if b.Side != WHITE {
-		dir *= -1
-	}
-
-	// TODO: implement SEE or MVV-LVA ordering
-	// Calculate the relative value of exchange
-	// from, to := move.FromTo()
-	// us, them := PieceWeights[b.Coords[from]], PieceWeights[b.Coords[to]]
-	// if them == 0 {
-	// 	value += 0
-	// } else {
-	// 	value += dir * (0.5*us + them)
-	// }
-
-	// Prioritize promotions
-	if move.Promotion() != 0 {
-		value += 3
-	}
-
-	return
 }
 
 func (b *Board) updateCastlingRights(move Move) {
